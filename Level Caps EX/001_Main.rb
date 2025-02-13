829273ffbdf74598d33469d64b47103b5bb49f7c
99Problemsx <132574224+99Problemsx@users.noreply.github.com>
2025-02-13 11:41:39 +0100

    ,

--- Level Caps EX/001_Main.rb
+++ Level Caps EX/001_Main.rb
@@ -1,209 +1,189 @@
-#-------------------------------------------------------------------------------
-# Hard Level Cap related Additions
-#-------------------------------------------------------------------------------
 class Pokemon
   def level=(value)
     validate value => Integer
-    if value < 1 || value > GameData::GrowthRate.max_level
-      max_lvl = GameData::GrowthRate.max_level
-      limit = (value < 1)? ["below the minimum  of level 1", "1"] : ["above the maximum of level #{max_lvl}", max_lvl.to_s]
-      echoln _INTL("Level {1} for {2} is not a valid level as it goes {3}. The level has been reset to {4}",
-                    value, self, limit[0], limit[1])
-      value.clamp(1, GameData::GrowthRate.max_level)
+    max_lvl = GameData::GrowthRate.max_level
+    if value < 1 || value > max_lvl
+      limit = (value < 1) ? ["below the minimum of level 1", "1"] : ["above the maximum of level #{max_lvl}", max_lvl.to_s]
+      echoln _INTL("Level {1} for {2} is not valid as it goes {3}. Reset to {4}", value, self, limit[0], limit[1])
+      value = value.clamp(1, max_lvl)
     end
     @exp = growth_rate.minimum_exp_for_level(value)
     @level = value
   end
 
-  def crosses_level_cap?
-    return LevelCapsEX.soft_cap? && self.level >= LevelCapsEX.level_cap
+  def crosses_level_cap?(is_player)
+    return false unless LevelCapsEX.applicable_level_cap?(is_player)
+    LevelCapsEX.soft_cap? && self.level >= LevelCapsEX.level_cap
   end
 
   def level
-    @level = growth_rate.level_from_exp(@exp) if !@level
+    @level ||= growth_rate.level_from_exp(@exp)
     self.level = GameData::GrowthRate.max_level if @level > GameData::GrowthRate.max_level
-    return @level
+    @level
+  end
+
+  def apply_exp(exp, is_player)
+    max_exp = growth_rate.minimum_exp_for_level(LevelCapsEX.level_cap)
+    exp = [exp, max_exp - @exp].min if crosses_level_cap?(is_player)
+    self.exp += exp
   end
 end
 
 module GameData
   class GrowthRate
     def self.max_level
-      return LevelCapsEX.hard_level_cap
+      LevelCapsEX.hard_level_cap
     end
   end
 end
 
-#-------------------------------------------------------------------------------
-# Soft Level Cap related Additions
-#-------------------------------------------------------------------------------
 class Battle
-
   def pbGainExpOne(idxParty, defeatedBattler, numPartic, expShare, expAll, showMessages = true)
-    pkmn = pbParty(0)[idxParty]   # The Pokémon gaining Exp from defeatedBattler
+    pkmn = pbParty(0)[idxParty]
     growth_rate = pkmn.growth_rate
-    # Don't bother calculating if gainer is already at max Exp
-    if pkmn.exp >= growth_rate.maximum_exp
-      pkmn.calc_stats   # To ensure new EVs still have an effect
-      return
-    end
-    isPartic    = defeatedBattler.participants.include?(idxParty)
+    return if pkmn.exp >= growth_rate.maximum_exp
+
+    isPartic = defeatedBattler.participants.include?(idxParty)
     hasExpShare = expShare.include?(idxParty)
     level = defeatedBattler.level
-    # Main Exp calculation
-    exp = 0
+    exp = calculate_exp(isPartic, hasExpShare, level, defeatedBattler, numPartic, expShare, expAll)
+    return if exp <= 0
+
+    exp = apply_exp_modifiers(exp, pkmn, defeatedBattler)
+    pkmn.apply_exp(exp, true)
+
+    show_exp_message(showMessages, pkmn, exp)
+    handle_level_up(pkmn, exp, idxParty)
+  end
+
+  private
+
+  def calculate_exp(isPartic, hasExpShare, level, defeatedBattler, numPartic, expShare, expAll)
     a = level * defeatedBattler.pokemon.base_exp
+    exp = 0
     if expShare.length > 0 && (isPartic || hasExpShare)
-      if numPartic == 0   # No participants, all Exp goes to Exp Share holders
-        exp = a / (Settings::SPLIT_EXP_BETWEEN_GAINERS ? expShare.length : 1)
-      elsif Settings::SPLIT_EXP_BETWEEN_GAINERS   # Gain from participating and/or Exp Share
-        exp = a / (2 * numPartic) if isPartic
-        exp += a / (2 * expShare.length) if hasExpShare
-      else   # Gain from participating and/or Exp Share (Exp not split)
-        exp = (isPartic) ? a : a / 2
-      end
-    elsif isPartic   # Participated in battle, no Exp Shares held by anyone
+      exp = if numPartic == 0
+              a / (Settings::SPLIT_EXP_BETWEEN_GAINERS ? expShare.length : 1)
+            elsif Settings::SPLIT_EXP_BETWEEN_GAINERS
+              (isPartic ? a / (2 * numPartic) : 0) + (hasExpShare ? a / (2 * expShare.length) : 0)
+            else
+              isPartic ? a : a / 2
+            end
+    elsif isPartic
       exp = a / (Settings::SPLIT_EXP_BETWEEN_GAINERS ? numPartic : 1)
-    elsif expAll   # Didn't participate in battle, gaining Exp due to Exp All
-      # NOTE: Exp All works like the Exp Share from Gen 6+, not like the Exp All
-      #       from Gen 1, i.e. Exp isn't split between all Pokémon gaining it.
+    elsif expAll
       exp = a / 2
     end
-    return if exp <= 0
-    # Pokémon gain more Exp from trainer battles
+    exp
+  end
+
+  def apply_exp_modifiers(exp, pkmn, defeatedBattler)
     exp = (exp * 1.5).floor if Settings::MORE_EXP_FROM_TRAINER_POKEMON && trainerBattle?
-    # Scale the gained Exp based on the gainer's level (or not)
-    if Settings::SCALED_EXP_FORMULA
-      exp /= 5
-      levelAdjust = ((2 * level) + 10.0) / (pkmn.level + level + 10.0)
-      levelAdjust **= 5
-      levelAdjust = Math.sqrt(levelAdjust)
-      exp *= levelAdjust
-      exp = exp.floor
-      exp += 1 if isPartic || hasExpShare
-    else
-      exp /= 7
-    end
-    # Foreign Pokémon gain more Exp
-    isOutsider = (pkmn.owner.id != pbPlayer.id ||
-                 (pkmn.owner.language != 0 && pkmn.owner.language != pbPlayer.language))
-    if isOutsider
-      if pkmn.owner.language != 0 && pkmn.owner.language != pbPlayer.language
-        exp = (exp * 1.7).floor
-      else
-        exp = (exp * 1.5).floor
-      end
-    end
-    # Exp. Charm increases Exp gained
+    exp = scaled_exp(exp, defeatedBattler, pkmn) if Settings::SCALED_EXP_FORMULA
+    exp = foreign_pokemon_exp(exp, pkmn)
     exp = exp * 3 / 2 if $bag.has?(:EXPCHARM)
-    # Modify Exp gain based on pkmn's held item
-    i = Battle::ItemEffects.triggerExpGainModifier(pkmn.item, pkmn, exp)
-    if i < 0
-      i = Battle::ItemEffects.triggerExpGainModifier(@initialItems[0][idxParty], pkmn, exp)
+    exp = Battle::ItemEffects.triggerExpGainModifier(pkmn.item, pkmn, exp) if Battle::ItemEffects.triggerExpGainModifier(pkmn.item, pkmn, exp) >= 0
+    exp = affection_level_exp(exp, pkmn) if Settings::AFFECTION_EFFECTS && @internalBattle && pkmn.affection_level >= 4 && !pkmn.mega?
+    exp
+  end
+
+  def scaled_exp(exp, defeatedBattler, pkmn)
+    exp /= 5
+    levelAdjust = ((2 * defeatedBattler.level) + 10.0) / (pkmn.level + defeatedBattler.level + 10.0)
+    levelAdjust **= 5
+    levelAdjust = Math.sqrt(levelAdjust)
+    (exp * levelAdjust).floor + 1
+  end
+
+  def foreign_pokemon_exp(exp, pkmn)
+    if (pkmn.owner.id != pbPlayer.id || (pkmn.owner.language != 0 && pkmn.owner.language != pbPlayer.language))
+      exp = (exp * 1.7).floor if pkmn.owner.language != 0 && pkmn.owner.language != pbPlayer.language
+      exp = (exp * 1.5).floor
     end
-    exp = i if i >= 0
-    # Boost Exp gained with high affection
-    if Settings::AFFECTION_EFFECTS && @internalBattle && pkmn.affection_level >= 4 && !pkmn.mega?
-      exp = exp * 6 / 5
-      isOutsider = true   # To show the "boosted Exp" message
-    end
-    # Modify exp gain based on soft level cap
-    over_level_cap = false
-    if pkmn.crosses_level_cap? && LevelCapsEX.level_cap_mode == 2
-      over_level_cap = true
-      exp = (exp / 10).to_i
-      exp = 1 if exp < 1
-    end
-    # Make sure Exp doesn't exceed the maximum
-    expFinal = growth_rate.add_exp(pkmn.exp, exp)
-    expGained = expFinal - pkmn.exp
-    return if expGained <= 0
-    # "Exp gained" message
+    exp
+  end
+
+  def affection_level_exp(exp, pkmn)
+    exp * 6 / 5
+  end
+
+  def show_exp_message(showMessages, pkmn, expGained)
     if showMessages
       message = _INTL("{1} got {2} Exp. Points!")
       message = _INTL("{1} got a boosted {2} Exp. Points!") if isOutsider
       message = _INTL("{1} got a reduced {2} Exp. Points!") if over_level_cap
       pbDisplayPaused(_INTL(message, pkmn.name, expGained))
     end
+  end
+
+  def handle_level_up(pkmn, expGained, idxParty)
     curLevel = pkmn.level
-    newLevel = growth_rate.level_from_exp(expFinal)
-    if newLevel < curLevel
-      debugInfo = "Levels: #{curLevel}->#{newLevel} | Exp: #{pkmn.exp}->#{expFinal} | gain: #{expGained}"
-      raise _INTL("{1}'s new level is less than its current level, which shouldn't happen.", pkmn.name) + "\n[#{debugInfo}]"
-    end
-    # Give Exp
-    if pkmn.shadowPokemon?
-      if pkmn.heartStage <= 3
-        pkmn.exp += expGained
-        $stats.total_exp_gained += expGained
-      end
-      return
-    end
+    newLevel = pkmn.growth_rate.level_from_exp(pkmn.exp)
+    raise_invalid_level_exception(pkmn, curLevel, newLevel, pkmn.exp, expGained) if newLevel < curLevel
+
     $stats.total_exp_gained += expGained
     tempExp1 = pkmn.exp
     battler = pbFindBattler(idxParty)
-    loop do   # For each level gained in turn...
-      # EXP Bar animation
-      levelMinExp = growth_rate.minimum_exp_for_level(curLevel)
-      levelMaxExp = growth_rate.minimum_exp_for_level(curLevel + 1)
-      tempExp2 = (levelMaxExp < expFinal) ? levelMaxExp : expFinal
+    level_up_loop(pkmn, tempExp1, pkmn.exp, curLevel, newLevel, battler)
+  end
+
+  def raise_invalid_level_exception(pkmn, curLevel, newLevel, expFinal, expGained)
+    debugInfo = "Levels: #{curLevel}->#{newLevel} | Exp: #{pkmn.exp}->#{expFinal} | gain: #{expGained}"
+    raise _INTL("{1}'s new level is less than its current level, which shouldn't happen.", pkmn.name) + "\n[#{debugInfo}]"
+  end
+
+  def level_up_loop(pkmn, tempExp1, expFinal, curLevel, newLevel, battler)
+    loop do
+      levelMinExp = pkmn.growth_rate.minimum_exp_for_level(curLevel)
+      levelMaxExp = pkmn.growth_rate.minimum_exp_for_level(curLevel + 1)
+      tempExp2 = [levelMaxExp, expFinal].min
       pkmn.exp = tempExp2
       @scene.pbEXPBar(battler, levelMinExp, levelMaxExp, tempExp1, tempExp2)
       tempExp1 = tempExp2
       curLevel += 1
-      if curLevel > newLevel
-        # Gained all the Exp now, end the animation
-        pkmn.calc_stats
-        battler&.pbUpdate(false)
-        @scene.pbRefreshOne(battler.index) if battler
-        break
-      end
-      # Levelled up
+      break if curLevel > newLevel
       pbCommonAnimation("LevelUp", battler) if battler
-      oldTotalHP = pkmn.totalhp
-      oldAttack  = pkmn.attack
-      oldDefense = pkmn.defense
-      oldSpAtk   = pkmn.spatk
-      oldSpDef   = pkmn.spdef
-      oldSpeed   = pkmn.speed
-      battler.pokemon.changeHappiness("levelup") if battler&.pokemon
+      old_stats = [pkmn.totalhp, pkmn.attack, pkmn.defense, pkmn.spatk, pkmn.spdef, pkmn.speed]
+      pkmn.changeHappiness("levelup")
       pkmn.calc_stats
       battler&.pbUpdate(false)
       @scene.pbRefreshOne(battler.index) if battler
       pbDisplayPaused(_INTL("{1} grew to Lv. {2}!", pkmn.name, curLevel)) { pbSEPlay("Pkmn level up") }
-      @scene.pbLevelUp(pkmn, battler, oldTotalHP, oldAttack, oldDefense,
-                       oldSpAtk, oldSpDef, oldSpeed)
-      # Learn all moves learned at this level
-      moveList = pkmn.getMoveList
-      moveList.each { |m| pbLearnMove(idxParty, m[1]) if m[0] == curLevel }
+      @scene.pbLevelUp(pkmn, battler, *old_stats)
+      learn_moves(pkmn, curLevel, idxParty)
     end
+  end
+
+  def learn_moves(pkmn, curLevel, idxParty)
+    pkmn.getMoveList.each { |m| pbLearnMove(idxParty, m[1]) if m[0] == curLevel }
   end
 end
 
-#-------------------------------------------------------------------------------
-# Obedience Related Level Cap Additions
-#-------------------------------------------------------------------------------
 class Battle::Battler
-
   alias __level_cap__pbObedienceCheck? pbObedienceCheck? unless method_defined?(:__level_cap__pbObedienceCheck?)
+
   def pbObedienceCheck?(*args)
     ret = __level_cap__pbObedienceCheck?(*args)
     db = @disobeyed
     @disobeyed = false
     return ret if ret || db
-    # Level Cap Disobedience checks
     return true if LevelCapsEX.level_cap_mode != 3
+    level_check(args[0])
+  end
+
+  def level_check(action)
     lv_diff = @level - LevelCapsEX.level_cap
     lv_diff = 5 if lv_diff >= 5
     disobedient = rand(5 - lv_diff) == 0
-    return pbDisobey(args[0], (lv_diff * 2)) if lv_diff >= 5 || disobedient
-    return true
+    return pbDisobey(action, (lv_diff * 2)) if lv_diff >= 5 || disobedient
+    true
   end
 
   alias __level_cap__pbDisobey pbDisobey unless method_defined?(:__level_cap__pbDisobey)
+
   def pbDisobey(*args)
     ret = __level_cap__pbDisobey(*args)
     @disobeyed = true
-    return ret
+    ret
   end
 end
--- Level Caps EX/002_Additions.rb
+++ Level Caps EX/002_Additions.rb
@@ -7,104 +7,89 @@ ItemHandlers::UseOnPokemonMaximum.add(:RARECANDY, proc { |item, pkmn|
 })
 
 ItemHandlers::UseOnPokemon.add(:RARECANDY, proc { |item, qty, pkmn, scene|
+  if pkmn.shadowPokemon? || pkmn.level >= GameData::GrowthRate.max_level
+    handle_max_level_or_shadow(scene, pkmn, item)
+  elsif pkmn.crosses_level_cap?(true)
+    scene.pbDisplay(_INTL("{1} refuses to eat the {2}.", pkmn.name, GameData::Item.get(item).name))
+    next false
+  else
+    pbSEPlay("Pkmn level up")
+    pbChangeLevel(pkmn, pkmn.level + qty, scene)
+    scene.pbHardRefresh
+    next true
+  end
+})
+
+def handle_max_level_or_shadow(scene, pkmn, item)
   if pkmn.shadowPokemon?
     scene.pbDisplay(_INTL("It won't have any effect."))
-    next false
-  elsif pkmn.level >= GameData::GrowthRate.max_level
+    false
+  else
     new_species = pkmn.check_evolution_on_level_up
     if !Settings::RARE_CANDY_USABLE_AT_MAX_LEVEL || !new_species
       scene.pbDisplay(_INTL("It won't have any effect."))
-      next false
+      false
+    else
+      evolve_pokemon(scene, pkmn, new_species)
+      true
     end
-    # Check for evolution
-    pbFadeOutInWithMusic do
-      evo = PokemonEvolutionScene.new
-      evo.pbStartScreen(pkmn, new_species)
-      evo.pbEvolution
-      evo.pbEndScreen
-      scene.pbRefresh if scene.is_a?(PokemonPartyScreen)
-    end
-    next true
-  elsif pkmn.crosses_level_cap?
-    scene.pbDisplay(_INTL("{1} refuses to eat the {2}.", pkmn.name, GameData::Item.get(item).name))
-    next false
   end
-  # Level up
-  pbSEPlay("Pkmn level up")
-  pbChangeLevel(pkmn, pkmn.level + qty, scene)
-  scene.pbHardRefresh
-  next true
-})
+end
+
+def evolve_pokemon(scene, pkmn, new_species)
+  pbFadeOutInWithMusic do
+    evo = PokemonEvolutionScene.new
+    evo.pbStartScreen(pkmn, new_species)
+    evo.pbEvolution
+    evo.pbEndScreen
+    scene.pbRefresh if scene.is_a?(PokemonPartyScreen)
+  end
+end
 
 #-------------------------------------------------------------------------------
 # EXP Candy Edits for Level Caps
 #-------------------------------------------------------------------------------
-ItemHandlers::UseOnPokemonMaximum.add(:EXPCANDYXS, proc { |item, pkmn|
-  gain_amount = 100
-  max_exp = LevelCapsEX.soft_cap? ? pkmn.growth_rate.minimum_exp_for_level(LevelCapsEX.level_cap) : pkmn.growth_rate.maximum_exp
-  next ((max_exp - pkmn.exp) / gain_amount.to_f).ceil
-})
+[:EXPCANDYXS, :EXPCANDYS, :EXPCANDYM, :EXPCANDYL, :EXPCANDYXL].each do |candy|
+  exp_candy_handler(candy)
+end
 
-ItemHandlers::UseOnPokemon.add(:EXPCANDYXS, proc { |item, qty, pkmn, scene|
-  next pbGainExpFromExpCandy(pkmn, 100, qty, scene, item)
-})
+def exp_candy_handler(candy)
+  gain_amount = candy_gain_amount(candy)
+  ItemHandlers::UseOnPokemonMaximum.add(candy, proc { |item, pkmn|
+    max_exp = LevelCapsEX.soft_cap? ? pkmn.growth_rate.minimum_exp_for_level(LevelCapsEX.level_cap) : pkmn.growth_rate.maximum_exp
+    next ((max_exp - pkmn.exp) / gain_amount.to_f).ceil
+  })
 
-ItemHandlers::UseOnPokemonMaximum.add(:EXPCANDYS, proc { |item, pkmn|
-  gain_amount = 800
-  max_exp = LevelCapsEX.soft_cap? ? pkmn.growth_rate.minimum_exp_for_level(LevelCapsEX.level_cap) : pkmn.growth_rate.maximum_exp
-  next ((max_exp - pkmn.exp) / gain_amount.to_f).ceil
-})
+  ItemHandlers::UseOnPokemon.add(candy, proc { |item, qty, pkmn, scene|
+    next pbGainExpFromExpCandy(pkmn, gain_amount, qty, scene, item)
+  })
+end
 
-ItemHandlers::UseOnPokemon.add(:EXPCANDYS, proc { |item, qty, pkmn, scene|
-  next pbGainExpFromExpCandy(pkmn, 800, qty, scene, item)
-})
-
-ItemHandlers::UseOnPokemonMaximum.add(:EXPCANDYM, proc { |item, pkmn|
-  gain_amount = 3_000
-  max_exp = LevelCapsEX.soft_cap? ? pkmn.growth_rate.minimum_exp_for_level(LevelCapsEX.level_cap) : pkmn.growth_rate.maximum_exp
-  next ((max_exp - pkmn.exp) / gain_amount.to_f).ceil
-})
-
-ItemHandlers::UseOnPokemon.add(:EXPCANDYM, proc { |item, qty, pkmn, scene|
-  next pbGainExpFromExpCandy(pkmn, 3_000, qty, scene, item)
-})
-
-ItemHandlers::UseOnPokemonMaximum.add(:EXPCANDYL, proc { |item, pkmn|
-  gain_amount = 10_000
-  max_exp = LevelCapsEX.soft_cap? ? pkmn.growth_rate.minimum_exp_for_level(LevelCapsEX.level_cap) : pkmn.growth_rate.maximum_exp
-  next ((max_exp - pkmn.exp) / gain_amount.to_f).ceil
-})
-
-ItemHandlers::UseOnPokemon.add(:EXPCANDYL, proc { |item, qty, pkmn, scene|
-  next pbGainExpFromExpCandy(pkmn, 10_000, qty, scene, item)
-})
-
-ItemHandlers::UseOnPokemonMaximum.add(:EXPCANDYXL, proc { |item, pkmn|
-  gain_amount = 30_000
-  max_exp = LevelCapsEX.soft_cap? ? pkmn.growth_rate.minimum_exp_for_level(LevelCapsEX.level_cap) : pkmn.growth_rate.maximum_exp
-  next ((max_exp - pkmn.exp) / gain_amount.to_f).ceil
-})
-
-ItemHandlers::UseOnPokemon.add(:EXPCANDYXL, proc { |item, qty, pkmn, scene|
-  next pbGainExpFromExpCandy(pkmn, 30_000, qty, scene, item)
-})
+def candy_gain_amount(candy)
+  case candy
+  when :EXPCANDYXS then 100
+  when :EXPCANDYS then 800
+  when :EXPCANDYM then 3_000
+  when :EXPCANDYL then 10_000
+  when :EXPCANDYXL then 30_000
+  end
+end
 
 def pbGainExpFromExpCandy(pkmn, base_amt, qty, scene, item)
   if pkmn.level >= GameData::GrowthRate.max_level || pkmn.shadowPokemon?
     scene.pbDisplay(_INTL("It won't have any effect."))
     return false
-  elsif pkmn.crosses_level_cap?
+  elsif pkmn.crosses_level_cap?(true)
     scene.pbDisplay(_INTL("{1} refuses to eat the {2}.", pkmn.name, GameData::Item.get(item).name))
     return false
+  else
+    pbSEPlay("Pkmn level up")
+    scene.scene.pbSetHelpText("") if scene.is_a?(PokemonPartyScreen)
+    (qty - 1).times { pkmn.changeHappiness("vitamin") } if qty > 1
+    pkmn.apply_exp(base_amt * qty, true)
+    scene.pbHardRefresh
+    return true
   end
-  pbSEPlay("Pkmn level up")
-  scene.scene.pbSetHelpText("") if scene.is_a?(PokemonPartyScreen)
-  if qty > 1
-    (qty - 1).times { pkmn.changeHappiness("vitamin") }
-  end
-  pbChangeExp(pkmn, pkmn.exp + (base_amt * qty), scene)
-  scene.pbHardRefresh
-  return true
 end
 
 #-------------------------------------------------------------------------------
@@ -112,27 +97,39 @@ end
 #-------------------------------------------------------------------------------
 class Game_Variables
   alias __level_caps__set_variable []= unless method_defined?(:__level_caps__set_variable)
+
   def []=(variable_id, value)
     old_value = self[variable_id]
     ret = __level_caps__set_variable(variable_id, value)
-    if value != old_value && LevelCapsEX::LOG_LEVEL_CAP_CHANGES
-      if variable_id == LevelCapsEX::LEVEL_CAP_VARIABLE
-        echoln "Current Level Cap updated from Lv. #{old_value} to Lv. #{value}"
-      elsif variable_id == LevelCapsEX::LEVEL_CAP_MODE_VARIABLE && self[LevelCapsEX::LEVEL_CAP_VARIABLE] != 0
-        mode_names = [
-          "None",
-          "Hard Cap",
-          "EXP Cap",
-          "Obedience Cap"
-        ]
-        old_name = mode_names[old_value] || "None"
-        new_name = mode_names[value] || "None"
-        echoln "Current Level Cap Mode updated from \"#{old_name}\" to \"#{new_name}\""
+    log_level_cap_changes(variable_id, old_value, value) if value != old_value && LevelCapsEX::LOG_LEVEL_CAP_CHANGES
+    display_level_cap_change_message(variable_id, value) if value != old_value && LevelCapsEX::DISPLAY_LEVEL_CAP_CHANGE_MESSAGE
+    ret
+  end
+
+  private
+
+  def log_level_cap_changes(variable_id, old_value, value)
+    if variable_id == LevelCapsEX::LEVEL_CAP_VARIABLE
+      echoln "Current Level Cap updated from Lv. #{old_value} to Lv. #{value}"
+    elsif variable_id == LevelCapsEX::LEVEL_CAP_MODE_VARIABLE && self[LevelCapsEX::LEVEL_CAP_VARIABLE] != 0
+      mode_names = ["None", "Hard Cap", "EXP Cap", "Obedience Cap"]
+      old_name = mode_names[old_value] || "None"
+      new_name = mode_names[value] || "None"
+      echoln "Current Level Cap Mode updated from \"#{old_name}\" to \"#{new_name}\""
+    end
+  end
+
+  class Game_Variables
+    alias_method :original_set_variable, :[]=
+  
+    def []=(variable_id, value)
+      old_value = self[variable_id]
+      original_set_variable(variable_id, value)
+      if variable_id == LevelCapsEX::LEVEL_CAP_VARIABLE && old_value != value && LevelCapsEX::DISPLAY_LEVEL_CAP_CHANGE_MESSAGE
+        pbMessage(_INTL("The level cap has been raised to Level {1}.", value))
       end
     end
-    return ret
   end
-end
 
 module Game
   class << self
@@ -149,35 +146,39 @@ end
 # Main Level Cap Module
 #-------------------------------------------------------------------------------
 module LevelCapsEX
-
   module_function
 
   def level_cap
-    return $game_variables[LEVEL_CAP_VARIABLE] if $game_variables && $game_variables[LEVEL_CAP_VARIABLE] > 0
-    return Settings::MAXIMUM_LEVEL
+    return Settings::MAXIMUM_LEVEL unless $game_variables && $game_variables[LEVEL_CAP_VARIABLE] > 0
+    $game_variables[LEVEL_CAP_VARIABLE]
   end
 
   def level_cap_mode
     lv_cap_mode = $game_variables[LEVEL_CAP_MODE_VARIABLE]
     return lv_cap_mode if $game_variables && [1, 2, 3].include?(lv_cap_mode)
-    return 0
+    0
   end
 
   def hard_cap?
-    return level_cap_mode == 1 && $game_variables[LEVEL_CAP_VARIABLE] > 0
+    level_cap_mode == 1 && $game_variables[LEVEL_CAP_VARIABLE] > 0
   end
 
   def soft_cap?
-    return [2, 3].include?(level_cap_mode) && $game_variables[LEVEL_CAP_VARIABLE] > 0
+    [2, 3].include?(level_cap_mode) && $game_variables[LEVEL_CAP_VARIABLE] > 0
   end
 
   def hard_level_cap
     max_lv = Settings::MAXIMUM_LEVEL
-    return max_lv if !$game_variables
+    return max_lv unless $game_variables
     lv_cap_mode = $game_variables[LEVEL_CAP_MODE_VARIABLE]
-    lv_cap = $game_variables[LevelCapsEX::LEVEL_CAP_VARIABLE]
-    return max_lv if lv_cap > max_lv 
-    return lv_cap if lv_cap > 0 && lv_cap_mode == 1
-    return max_lv
+    lv_cap = $game_variables[LEVEL_CAP_VARIABLE]
+    return max_lv if lv_cap > max_lv
+    lv_cap > 0 && lv_cap_mode == 1 ? lv_cap : max_lv
+  end
+
+  def applicable_level_cap?(is_player)
+    return true if is_player && $game_switches[APPLY_TO_PLAYER_SWITCH]
+    return true if !is_player && $game_switches[APPLY_TO_OPPONENT_SWITCH]
+    false
   end
 end
